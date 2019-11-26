d30f82f9148d241482d2a03373dd013cbfe0e222
Simon Barnett <si@medatechuk.com>
2019-11-26 13:48:02 +0000

    Added postchar function

--- post.vb
+++ post.vb
@@ -54,6 +54,52 @@ Partial Public Class UserDefinedFunctions
     End Function
 
     <Microsoft.SqlServer.Server.SqlFunction()>
+    Public Shared Function postchar(url As SqlString, poststr As SqlString) As SqlString
+
+        Dim requestStream As Stream = Nothing
+        Dim uploadResponse As HttpWebResponse = Nothing
+        Dim uploadRequest As HttpWebRequest = Nothing
+
+        Try
+            uploadRequest = CType(HttpWebRequest.Create(CType(url, String)), HttpWebRequest)
+            With uploadRequest
+                .Method = "POST"
+                .Proxy = Nothing
+                .ContentType = "text/xml"
+            End With
+
+            Dim myEncoder As New Text.ASCIIEncoding
+            Dim ms As MemoryStream = New MemoryStream(myEncoder.GetBytes(poststr.Value))
+
+            requestStream = uploadRequest.GetRequestStream()
+
+            ' Upload the XML
+            Dim buffer(1024) As Byte
+            Dim bytesRead As Integer
+            While True
+                bytesRead = ms.Read(buffer, 0, buffer.Length)
+                If bytesRead = 0 Then
+                    Exit While
+                End If
+                requestStream.Write(buffer, 0, bytesRead)
+            End While
+
+            requestStream.Close()
+            uploadResponse = CType(uploadRequest.GetResponse(), HttpWebResponse)
+
+            Dim sr As New StreamReader(uploadResponse.GetResponseStream)
+            With sr
+                Return .ReadToEnd
+            End With
+
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+
+        End Try
+
+    End Function
+
+    <Microsoft.SqlServer.Server.SqlFunction()>
     Public Shared Function getxml(url As SqlString) As SqlXml
         Try
             Dim requestStream As Stream = Nothing
